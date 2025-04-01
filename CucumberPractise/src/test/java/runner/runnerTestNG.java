a04632131b5ef86d8d5983f2c3647201e64247af
DhanusheswaranM <mdhanusheswaran@gmail.com>
2025-04-01 22:28:20 +0530

    01-04-2025

--- CucumberPractise/src/test/java/runner/runnerTestNG.java
+++ CucumberPractise/src/test/java/runner/runnerTestNG.java
@@ -2,11 +2,13 @@ package runner;
 
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.Test;
 
-
-@CucumberOptions(plugin= {"pretty" , "html:target/cucumber-reports/CucumberForSauceDemoForTestNG.html" }, 
-features="src/test/resources/com/features/SauceDemo.feature" , 
-glue="stepDefinitions")
+@CucumberOptions(
+    plugin = {"pretty", "html:target/cucumber-reports/CucumberForSauceDemoForTestNG.html"},
+    features = "src/test/resources/com/features/SauceDemo.feature",
+    glue = "stepDefinitions"
+)
 public class runnerTestNG extends AbstractTestNGCucumberTests {
 
 }
